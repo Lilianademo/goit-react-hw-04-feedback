0538300ff0db5600ca0a34c2319451855f048724
Lilianademo <123247173+Lilianademo@users.noreply.github.com>
2024-03-06 11:58:26 +0000

    Deploying to gh-pages from @ Lilianademo/goit-react-hw-04-feedback@3895d5a648ec036682ab9d2d85f1425ef01d4bba 🚀

--- src/components/Notification.jsx
+++
@@ -1,9 +0,0 @@
-import PropTypes from "prop-types";
-
-export const Notification = ({ message }) => {
-  return <h1>{message}</h1>;
-};
-
-Notification.propTypes = {
-  message: PropTypes.string.isRequired,
-};
